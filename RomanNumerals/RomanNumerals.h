4765d05d0595fcd175f1e4454180aaccee098ede
Ken Power <github@kenpower.com>
2020-11-30 15:35:55 +0000

    Refactor into static arrays

--- RomanNumerals/RomanNumerals.h
+++ RomanNumerals/RomanNumerals.h
@@ -2,32 +2,14 @@
 #include <string>
 #include <vector>
 
-static std::string unitsDigit(int digit)
-{
-	std::vector<std::string> unitsAsString{ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-	return unitsAsString[digit];
-}
-
-static std::string tensDigit(int digit)
-{
-	std::vector<std::string> tensAsString{ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-	return tensAsString[digit];
-}
-
-static std::string hundredsDigit(int digit)
-{
-	std::vector<std::string> hundredsAsString{ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-	return hundredsAsString[digit];
-}
-
-static std::string thousandsDigit(int digit)
-{
-	std::vector<std::string> thousandsAsString{ "", "M", "MM", "MMM" };
-	return thousandsAsString[digit];
-}
-
 class RomanNumerals
 {
+private:
+	static const std::vector<std::string> unitsAsString;
+	static const std::vector<std::string> tensAsString;
+	static const std::vector<std::string> hundredsAsString;
+	static const std::vector<std::string> thousandsAsString;
+
 public:
 	static std::string convertFromInteger(int number) {
 		std::string romanNumeral{ "" };
@@ -37,12 +19,17 @@ public:
 		int hundreds = ((number % 1000) - tens*10 -units) / 100;
 		int thousands = ((number % 10000) - hundreds*100 - tens * 10 - units) / 1000;
 
-		romanNumeral.append(thousandsDigit(thousands));
-		romanNumeral.append(hundredsDigit(hundreds));
-		romanNumeral.append(tensDigit(tens));
-		romanNumeral.append(unitsDigit(units));
+		romanNumeral.append(thousandsAsString[thousands]);
+		romanNumeral.append(hundredsAsString[hundreds]);
+		romanNumeral.append(tensAsString[tens]);
+		romanNumeral.append(unitsAsString[units]);
 
 		return romanNumeral;
 	}
 };
+
+const std::vector<std::string> RomanNumerals::thousandsAsString{ "", "M", "MM", "MMM" };
+const std::vector<std::string> RomanNumerals::hundredsAsString{ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+const std::vector<std::string> RomanNumerals::tensAsString{ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+const std::vector<std::string> RomanNumerals::unitsAsString{ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
